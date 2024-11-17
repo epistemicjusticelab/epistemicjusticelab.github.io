b357834cf68603d38b6f6f46f15a5cf0aef9d68c
Radhakrishnan <radhakrishnan@email.com>
2024-11-17 16:35:32 -0500

    initial commit

---
+++ src/content/pages/about.md
@@ -0,0 +1,33 @@
+---
+title: About
+seo:
+  title: About Me
+  description: Learn more about the person behind the website and embark on a journey of inspiration and shared experiences.
+  image:
+    src: '/about.jpeg'
+    alt: A person sitting at a desk in front of a computer
+---
+
+![Alt text for image](/SmallBioPic.jpg)
+
+## Angel Ford, Assitant Professor, CEHC
+
+### Education
+
+- EdD Curriculum Development and Instruction, Liberty University
+
+- MA Teaching and Learning, Liberty University
+
+- BA Psychology, Saint Leo University
+
+### Bio
+
+I joined the College of Emergency Preparedness, Homeland Security and Cybersecurity (CEHC) at the University at Albany in July 2023. She arrives with a diverse set of professional, teaching, and research experiences including positions as an Assistant Professor of Educational Research, and as a researcher in several higher education programs and partnerships. In 2019-2020, Dr. Ford was a Fulbright Scholar to Addis Ababa University in Ethiopia, teaching and conducting research. While she was in Ethiopia, she also taught PhD research courses in Information Science for Jimma University where she continues to support their growing Information Science program.
+
+Previous to entering the field of higher education, she served in K-12 classrooms and as a K-12 administrator and prior to that she served as an aircraft electro-environmental technician in the U.S. Air Force, where she enjoyed working on A-10, F-15, and F-16 aircrafts.
+
+Her education includes an EdD in Curriculum Development and Instruction and a MA in Teaching and Learning from Liberty University, a BA in Psychology from Saint Leo University, and an A.S. in Aircraft Systems Maintenance Technology. She has also completed several courses for a PhD in Educational Psychology and Program Evaluation from Old Dominion University.
+
+She currently (2023-2025) serves as the past president of the International Society for Educational Planning https://isep.info/. She also serves as a support to the Fulbright organization for the orientation of individuals about to embark on their experiences abroad. She is currently on a three-year tenure on the Fulbright Specialist roster where she hopes to complete projects related to building human capacity in research methods and scholarly communication in sub-Saharan Africa.
+
+Her research interests are closely tied to areas of equity both within the United States and internationally. These interests include improving learning environments; students and educator motivations, aspirations, well-being, and persistence; fair and equitable partnerships and collaborations; global social justice in scholarly communication, and the benefits of cross-disciplinary/national and international investigations. Dr. Ford will be establishing a research lab within CEHC based on her current research focus on global epistemic justice in scholarly communication.
---
+++ src/data/site-config.ts
@@ -0,0 +1,122 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Epistemic Justice Lab',
+    subtitle: 'CEHC',
+    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'News',
+            href: '/news'
+        }
+        // ,
+        // {
+        //     text: 'Tags',
+        //     href: '/tags'
+        // }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+        {
+            text: 'Terms',
+            href: '/terms'
+        },
+        {
+            text: 'Download theme',
+            href: 'https://github.com/JustGoodUI/dante-astro-theme'
+        }
+    ],
+    socialLinks: [
+        {
+            text: 'Dribbble',
+            href: 'https://dribbble.com/'
+        },
+        {
+            text: 'Instagram',
+            href: 'https://instagram.com/'
+        },
+        {
+            text: 'X/Twitter',
+            href: 'https://twitter.com/'
+        }
+    ],
+    hero: {
+        title: 'Moto',
+        text: "Research interests are closely tied to areas of equity both within the United States and internationally. These interests include improving learning environments; students and educator motivations, aspirations, well-being, and persistence; fair and equitable partnerships and collaborations; global social justice in scholarly communication, and the benefits of cross-disciplinary/national and international investigations.",
+        image: {
+            src: '/TransparentWhite.png',
+            alt: 'A person sitting at a desk in front of a computer'
+        },
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            }
+        ]
+    },
+    subscribe: {
+        title: 'Subscribe to Dante Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#'
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
